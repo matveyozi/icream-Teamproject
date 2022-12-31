4e6d70722b75b28e8a917fd6784b24b3d7da6066
matveyozi <106310421+matveyozi@users.noreply.github.com>
2022-12-31 09:37:31 +0000

    Deploying to gh-pages from @ matveyozi/icream-Teamproject@415afc35cc0c0f0f2e2ab3b3bf69f54fdc058a5c 🚀

---
+++ index.7acc5b4b.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:400,spaceBetween:100,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3,disableOnInteraction:!1}});
+//# sourceMappingURL=index.7acc5b4b.js.map
---
+++ index.a65457de.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:400,spaceBetween:100,pagination:{el:".swiper-pagination",clickable:!0},autoplay:{delay:3e3,disableOnInteraction:!1}});
+//# sourceMappingURL=index.a65457de.js.map
